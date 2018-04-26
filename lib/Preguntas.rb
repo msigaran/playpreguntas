b2bc8b4740108e5e81561e902ba746de1e0b7623
vagrant <vagrant@kleer-csd-box>
2018-04-26 19:29:14 -0300

    primera pregunta y primera respuesta

--- lib/Preguntas.rb
+++ lib/Preguntas.rb
@@ -1,8 +1,20 @@
 class Preguntas
 	def initialize
 		@Inicio="Buena suerte!!!."
+		@TodasPreguntas	=""
+		@TodasRespuestas=""
+		
 	end 
 	def mensaje_inicio 
 		return @Inicio
 	end 
+	
+	def preguntas
+		@TodasPreguntas= ["Todos somos aficionados. La vida es tan corta que no da para más"]
+		return @TodasPreguntas[0]		
+	end
+	def respuestas
+		@TodasRespuestas= ["Charles Chaplin", "Albert Eintein","Paulo Cohelo"]
+		return @TodasRespuestas[0]
+	end
 end
--- spec/preguntas_spec.rb
+++ spec/preguntas_spec.rb
@@ -1,14 +1,18 @@
 require "./lib/Preguntas.rb"
 
-
 describe "JUEGO PREGUNTAS" do
 	it "INICIA JUEGO" do
 		pregunta = Preguntas.new
 		pregunta.mensaje_inicio.should == "Buena suerte!!!." 
 	end
-#	it "Leer primera pregunta" do
-#		pregunta = Pregunta.new
-#		partido.puntaje.should == "" 
-#	end
+
+	it "Leer primera pregunta" do
+		pregunta = Preguntas.new
+		pregunta.preguntas.should == "Todos somos aficionados. La vida es tan corta que no da para más" 
+	end
+	it "Leer primera respuesta" do
+		respuesta = Preguntas.new
+		respuesta.respuestas.should == "Charles Chaplin" 
+	end
 
 end
